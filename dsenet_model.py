4f540e477eb5f82c6c142067302d2c2b8d136ea0
1605066114 <73947793+1605066114@users.noreply.github.com>
2022-09-27 17:29:28 +0800

    Update dsenet_model.py

--- dsenet_model.py
+++ dsenet_model.py
@@ -1,11 +1,9 @@
-""" Full assembly of the parts to form the complete network """
-"""Refer https://github.com/milesial/Pytorch-UNet/blob/master/unet/unet_model.py"""
 
 import torch.nn.functional as F
 
-from .dsenet_parts import *
+from dsenet_parts import *
 
-
+#Form each block into a DSENet as described in the paper
 class Dsenet(nn.Module):
     def __init__(self, n_channels, n_classes, bilinear=True):
         super(Dsenet, self).__init__()
@@ -13,21 +11,21 @@ class Dsenet(nn.Module):
         self.n_classes = n_classes
         self.bilinear = bilinear
 
-        self.inc = TripleConv(n_channels, 16)
-        self.down1 = Down(16, 32)
-        self.down2 = Down(32, 64)
-        self.down3 = Down(64, 128)
-        self.down4 = Down(128, 256)
-        self.down5 = Down(256, 512)
-        self.down6 = Down(512, 1024)
+        self.inc = TripleConv(n_channels, 16) #384×256×16
+        self.down1 = Down(16, 32)            #192×128×32
+        self.down2 = Down(32, 64)            #96×64×64
+        self.down3 = Down(64, 128)           #48×32×128
+        self.down4 = Down(128, 256)          #24×16×256
+        self.down5 = Down(256, 512)          #12×8×512
+        self.down6 = Down(512, 1024)         #6×4×1024
 
-        self.up1 = Up(1536, 512, bilinear)
-        self.up2 = Up(768, 256, bilinear)
-        self.up3 = Up(384, 128, bilinear)
-        self.up4 = Up(192, 64, bilinear)
-        self.up5 = Up(96, 32, bilinear)
-        self.up6 = Up(48, 16, bilinear)
-        self.outc = OutConv(16, n_classes)
+        self.up1 = Up(1536, 512, bilinear)  #12×8×512
+        self.up2 = Up(768, 256, bilinear)   #24×16×256
+        self.up3 = Up(384, 128, bilinear)   #48×32×128
+        self.up4 = Up(192, 64, bilinear)    #96×64×64
+        self.up5 = Up(96, 32, bilinear)     #192×128×32
+        self.up6 = Up(48, 16, bilinear)     #384×256×16
+        self.outc = OutConv(16, n_classes)  #384×256×1
 
     def forward(self, x):
         x1 = self.inc(x)
