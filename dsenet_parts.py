4f3b1cbad110f0716a39a423accdfa6a4d69943d
1605066114 <73947793+1605066114@users.noreply.github.com>
2022-10-20 09:28:50 +0800

    Update dsenet_parts.py

--- dsenet_parts.py
+++ dsenet_parts.py
@@ -13,7 +13,6 @@ class TripleConv(nn.Module):
             nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
-            nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
             nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
             nn.ReLU(inplace=True)
